4da9bbfba148061f1bc90c0b79c519e5f21c1bdc
Stere Marius <stere.marius99@gmail.com>
2021-06-15 18:27:36 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import formRoutes from "./routes/formRoutes.js";
 import groupRoutes from "./routes/groupRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import morgan from "morgan";
+import path from "path";
 
 dotenv.config();
 
@@ -14,16 +15,29 @@ connectDB();
 
 const app = express();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("API is running");
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/forms", formRoutes);
 app.use("/api/groups", groupRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (request, response) =>
+    response.sendFile(
+      path.resolve(__dirname, "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (request, response) => {
+    response.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
