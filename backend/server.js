3c4bec17279e805826e01448047a0112b9895491
Stere Marius <stere.marius99@gmail.com>
2021-06-15 18:48:33 +0300

    Sterge metoda listen

--- backend/server.js
+++ backend/server.js
@@ -42,11 +42,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
+// app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+//   )
+// );
